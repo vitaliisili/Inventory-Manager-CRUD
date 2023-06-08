4bf45a281ede01b3c4735b8988464c1ddfcd6e19
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-08 22:02:25 +0200

    APP --> add fast api

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .controller import protuct_controller
+
+app = FastAPI()
+app.include_router(protuct_controller.router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
