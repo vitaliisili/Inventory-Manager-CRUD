32731efc4003a04b363ab7043cc79e22eb308ffa
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-08 22:04:35 +0200

    APP --> add database configuration

---
+++ app/config/database_config.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .app_config import settings
+from ..models import product_model
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@' \
+                          f'{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+product_model.Base.metadata.create_all(bind=engine)
