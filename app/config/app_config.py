09d616e068b7e288e8dee21639b8ed0f66b6910b
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-08 22:03:42 +0200

    APP --> create app configuration file

---
+++ app/config/app_config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_password: str
+    database_username: str
+    database_hostname: str
+    database_port: str
+    database_name: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
