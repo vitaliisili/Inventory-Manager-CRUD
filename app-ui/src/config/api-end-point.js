57c5255f461d3a8a410f7ed6ed7f7df6715ea0ef
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-08 21:55:44 +0200

    UI --> add api end point

---
+++ app-ui/src/config/api-end-point.js
@@ -0,0 +1,11 @@
+const PORT = "8000"
+const API_VERSION = 'api'
+const HOST = `http://localhost:${PORT}/${API_VERSION}`
+
+export const API_GET_ALL_PRODUCTS = `${HOST}/products`
+export const API_GET_PRODUCT_BY_ID = `${HOST}/products/{0}`
+export const API_POST_PRODUCT = `${HOST}/products`
+export const API_UPDATE_PRODUCT = `${HOST}/products/{0}`
+export const API_DELETE_PRODUCT = `${HOST}/products/{0}`
+export const API_GET_PRODUCT_TOTAL_VALUE = `${HOST}/products/total-value`
+export const API_GET_PRODUCT_TOTAL_ITEMS = `${HOST}/products/total-items`
