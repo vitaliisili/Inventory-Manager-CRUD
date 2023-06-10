e5e3be6e12b7d812c145900bb4f6876d324478a1
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-11 01:46:54 +0200

    fix docker deploy

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     database_password: str
     database_username: str
     database_hostname: str
-    database_port: str
+    # database_port: str
     database_name: str
 
     class Config:
