e74a7badc4d28e45d7e7ded1ad3a2206683929ee
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-11 13:33:43 +0200

    add backend url env

--- app-ui/src/config/api-end-point.js
+++ app-ui/src/config/api-end-point.js
@@ -1,7 +1,8 @@
 // const PORT = "8000"
 const API_VERSION = 'api'
 // const HOST = `http://localhost:${PORT}/${API_VERSION}` // API host server url
-const HOST = `https://inventoryapi.vitaliisili.com/${API_VERSION}` // API host server url
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+const HOST = `${BACKEND_URL}/${API_VERSION}` // API host server url
 
 export const API_GET_ALL_PRODUCTS = `${HOST}/products`
 export const API_GET_PRODUCT_BY_ID = `${HOST}/products/{0}`
