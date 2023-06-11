e0bdbe416942c2e371c104cc3f47b22eab4772e4
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-11 11:12:14 +0200

    fix production deploy

--- app-ui/src/config/api-end-point.js
+++ app-ui/src/config/api-end-point.js
@@ -1,12 +1,12 @@
 const PORT = "8000"
-const API_VERSION = 'api'
+// const API_VERSION = 'api'
 // const HOST = `http://localhost:${PORT}/${API_VERSION}` // API host server url
-const HOST = `vitaliisili.com:${PORT}/${API_VERSION}` // API host server url
+// const HOST = `vitaliisili.com:${PORT}/` // API host server url
 
-export const API_GET_ALL_PRODUCTS = `${HOST}/products`
-export const API_GET_PRODUCT_BY_ID = `${HOST}/products/{0}`
-export const API_POST_PRODUCT = `${HOST}/products`
-export const API_UPDATE_PRODUCT = `${HOST}/products/{0}`
-export const API_DELETE_PRODUCT = `${HOST}/products/{0}`
-export const API_GET_PRODUCT_TOTAL_VALUE = `${HOST}/products/total-value`
-export const API_GET_PRODUCT_TOTAL_ITEMS = `${HOST}/products/total-items`
+export const API_GET_ALL_PRODUCTS = `/products`
+export const API_GET_PRODUCT_BY_ID = `/products/{0}`
+export const API_POST_PRODUCT = `/products`
+export const API_UPDATE_PRODUCT = `/products/{0}`
+export const API_DELETE_PRODUCT = `/products/{0}`
+export const API_GET_PRODUCT_TOTAL_VALUE = `/products/total-value`
+export const API_GET_PRODUCT_TOTAL_ITEMS = `/products/total-items`
