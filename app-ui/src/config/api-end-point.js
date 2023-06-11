04ca3a496656b38b0acc926fd6044158821ae8e0
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-11 16:53:59 +0200

    fix rect api url

--- app-ui/src/config/api-end-point.js
+++ app-ui/src/config/api-end-point.js
@@ -1,10 +1,4 @@
 const API_VERSION = 'api'
-// https://inventoryapi.vitaliisili.com
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
-console.log("//////////////////")
-console.log(BACKEND_URL)
-console.log("//////////////////")
-// const HOST = `${BACKEND_URL}/${API_VERSION}` // API host server url
 const HOST = `http://localhost:8000/${API_VERSION}` // API host server url
 
 export const API_GET_ALL_PRODUCTS = `${HOST}/products`
