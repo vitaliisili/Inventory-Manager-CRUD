78083bfa95ed4dbb510a355ab34b6c63a2cac9cb
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-22 10:47:40 +0200

    test jenkins environment

--- app/config/database_config.py
+++ app/config/database_config.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from .app_config import settings
 from ..models import product_model
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:test@' \
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@' \
                           f'{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
