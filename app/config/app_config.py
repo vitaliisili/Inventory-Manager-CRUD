030582f06f7e03bc14883f4d0b02ffaedada34d6
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-25 16:13:04 +0200

    add env variable database_port_test for testing

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     database_password: str
     database_username: str
     database_hostname: str
-    database_port_test: str
+    database_portt: str
     database_port: str
     database_name: str
 
