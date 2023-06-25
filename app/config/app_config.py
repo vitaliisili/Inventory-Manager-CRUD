be63bee993c6622e827f442fc9dedf279dc3fa2e
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-25 16:15:38 +0200

    add env variable database_port_test for testing

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -1,19 +1,21 @@
 from pathlib import Path
 from pydantic import BaseSettings
+from dotenv import load_dotenv
 
-env_location = Path("./.env").resolve()
+load_dotenv()
+# env_location = Path("./.env").resolve()
 
 
 class Settings(BaseSettings):
     database_password: str
     database_username: str
     database_hostname: str
-    database_portt: str
+    database_port_test: str
     database_port: str
     database_name: str
 
     class Config:
-        env_file = env_location
+        env_file = '.env'
 
 
 settings = Settings()
