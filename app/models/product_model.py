202b45cbd0905c8456aeae35486ff5354ba55a3d
Vitalii Sili <vitaliisili@yahoo.com>
2023-06-25 14:19:33 +0200

    fix deprecated declarative_base import

--- app/models/product_model.py
+++ app/models/product_model.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Float, Integer
 from .base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
